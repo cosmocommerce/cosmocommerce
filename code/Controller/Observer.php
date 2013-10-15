cd6fa588da2b88324092e052613657b82d4e9665
Cosmo Mao <mao@excecbc.com>
2013-10-15 15:26:56 +0800

    fix design

--- code/Controller/Observer.php
+++ code/Controller/Observer.php
@@ -5,8 +5,19 @@ class CosmoCommerce_ThemeFix_Controller_Observer
 	//Event: adminhtml_controller_action_predispatch_start
 	public function overrideTheme()
 	{
+        (Mage::getSingleton('core/design_package')->setTheme('CosmoCommerce'));
+        /*
+        (Mage::getSingleton('core/design_package')->setPackageName('CosmoCommerce'));
+        (Mage::getSingleton('core/design_package')->setTheme('default'));
+        exit();
+		Mage::getDesign()->setArea('adminhtml')
+            ->setPackageName('CosmoCommerce') //Name of Package
+			->setTheme('default');
+        print_r(Mage::getDesign()->getArea());
+        exit();
 		Mage::getDesign()->setArea('adminhtml')
 			->setTheme((string)Mage::getStoreConfig('design/admin/theme'));
+        */
 //		Mage::getDesign()->setArea('adminhtml')->setTheme('skin', 'enterprise');
 	}
 }
